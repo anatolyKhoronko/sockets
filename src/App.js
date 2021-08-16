bdd9420590b8c0c8f9c9471fe7ed3d3671966604
Anatolii <anatolykhoronko@gmail.com>
2021-08-16 10:38:19 +0300

    test sockets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import logo from './logo.svg';
+import React, { useEffect} from 'react';
+import { io } from 'socket.io-client';
 import './App.css';
 
 function App() {
+
+	useEffect(() => {
+   const socket = io('https://dev-api-brella.wlcmapps.com', {
+     transports: ['websocket'],
+     reconnection: true,
+     // reconnectionDelayMax: 1000,
+     auth: {
+       token: '123',
+     },
+     query: {
+       'my-key': 'my-value',
+     },
+   });
+  console.log(socket);
+  setTimeout(() => {
+   console.log('Connected', socket.connected);
+  }, 3000);
+  socket.emit('joinLocationUpdates', '123');
+  socket.onAny(console.log)
+ }, []);
+
   return (
     <div className="App">
       <header className="App-header">
